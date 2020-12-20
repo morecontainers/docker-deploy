05c6a5197752db2eb62e57980a5df4af3af79b7a
Henrik Holst <hholst80@users.noreply.github.com>
2020-12-20 17:12:14 +0100

    Draft workflow for container redeployment
    No check if deployment is needed

--- src/docker-deploy.cr
+++ src/docker-deploy.cr
@@ -13,6 +13,10 @@ module Docker::Deploy
     exit(1)
   end
 
+  live = ""
+  staged = ""
+  silent = false
+
   # TODO: Put your code here
   OptionParser.parse do |parser|
     parser.banner = "Docker Deploy"
@@ -24,6 +28,9 @@ module Docker::Deploy
     parser.on "-h", "--help", "Show help" do
       puts parser
       exit
+    end
+    parser.on "-s", "--silent", "Silent operation" do
+      silent = true
     end
     parser.missing_option do |option_flag|
       STDERR.puts "ERROR: #{option_flag} is missing something."
@@ -40,7 +47,6 @@ module Docker::Deploy
       if unknown_args.size == 2
         live = unknown_args[0]
         staged = unknown_args[1]
-        puts "live=#{live} staged=#{staged}"
       else
         # invalid arguments
         STDERR.puts "ERROR: invalid arguments"
@@ -48,4 +54,21 @@ module Docker::Deploy
       end
     end
   end
+  
+  begin
+    live_container = client.containers.get live
+    staged_container = client.containers.get staged
+    puts "stop #{live}" unless silent
+    live_container.stop
+    puts "remove #{live}" unless silent
+    live_container.remove
+    puts "rename #{staged} -> #{live}" unless silent
+    staged_container.rename live
+    puts "start #{live}" unless silent
+    staged_container.start
+  rescue ex
+    STDERR.puts "ERROR: #{ex.message}"
+    exit 1
+  end
+
 end
