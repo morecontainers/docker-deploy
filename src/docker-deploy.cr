c23b4862a51ea4f32a6caf3d7e91272ae4e79cfb
Henrik Holst <hholst80@users.noreply.github.com>
2020-12-20 16:04:24 +0100

    Update docker-deploy.cr

--- src/docker-deploy.cr
+++ src/docker-deploy.cr
@@ -1,8 +1,17 @@
 # TODO: Write documentation for `Docker::Deploy`
 require "option_parser"
+require "docker"
 
 module Docker::Deploy
   VERSION = "0.1.0"
+
+  begin
+    client = Docker.client
+  rescue ex
+    STDERR.puts "Could not connect to Docker"
+    STDERR.puts ex.message
+    exit(1)
+  end
 
   # TODO: Put your code here
   OptionParser.parse do |parser|
@@ -26,6 +35,17 @@ module Docker::Deploy
       STDERR.puts "ERROR: #{option_flag} is not a valid option."
       STDERR.puts parser
       exit(1)
+    end
+    parser.unknown_args do |unknown_args|
+      if unknown_args.size == 2
+        live = unknown_args[0]
+        staged = unknown_args[1]
+        puts "live=#{live} staged=#{staged}"
+      else
+        # invalid arguments
+        STDERR.puts "ERROR: invalid arguments"
+        exit 1
+      end
     end
   end
 end
